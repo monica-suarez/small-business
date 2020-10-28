25ffa1991b4f963d8b5aac7ecd40dab0cb79789f
monica-suarez <shutterbugmo@gmail.com>
2020-10-27 21:03:55 -0500

    added files to project

---
+++ src/Router.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Switch, Route } from "react-router";
+
+const Router = () => {
+  return (
+    <Switch>
+      <Route />
+    </Switch>
+  );
+};
+
+export default Router;
